374f7a1f8f709da04374580c74643ac1629bf9a1
Ankit Kumar <ankitsingh31711@gmail.com>
2025-01-20 21:32:11 +0530

    ..

--- Class and Object.java
+++ Class and Object.java
@@ -1,15 +1,12 @@
 class Person {
-    // Instance variables
     String name;
     int age;
 
-    // Constructor
     Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Method to display person's details
     void displayInfo() {
         System.out.println("Name: " + name + ", Age: " + age);
     }
@@ -17,10 +14,8 @@ class Person {
 
 public class PersonExample {
     public static void main(String[] args) {
-        // Create an object of the Person class
         Person person1 = new Person("Ankit", 25);
 
-        // Call the method to display details
         person1.displayInfo();
     }
 }
