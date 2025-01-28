494e5d42fafdea632432a833ad1db666722a47af
Ankit Kumar <ankitsingh31711@gmail.com>
2025-01-28 23:45:29 +0530

    ..

---
+++ Palindrome number check.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Store the original number
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        // Reverse the number
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        // Check if the original number is equal to the reversed number
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " is a palindrome.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
